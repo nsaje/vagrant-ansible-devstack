cd1e03356f516caa07c16b82c67da4ac5a4fcb4c
Xiao Hanyu <xiaohanyu1988@gmail.com>
2013-10-29 21:05:26 +0800

    Fix quantum->neutron rename. Use precise 64 as the default box.

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "hackerbox_precise64"
+  config.vm.box = "precise64"
   # config.vm.box_url = "https://www.dropbox.com/s/uzuc3h5p7tysin2/hackerbox_precise64.box"
 
   config.vm.network :private_network, ip: "192.168.27.100"
--- devstack/dowhatyoulike.sh
+++ devstack/dowhatyoulike.sh
@@ -1,27 +1,16 @@
-cp /etc/apt/sources.list{,.$(date +%F_%T)}
+# You can put anything you like in this script to execute when vm bootup.
 
-cat <<EOF > /etc/apt/sources.list
-deb http://mirror.bjtu.edu.cn/ubuntu/ precise main multiverse restricted universe
-deb http://mirror.bjtu.edu.cn/ubuntu/ precise-backports main multiverse restricted universe
-deb http://mirror.bjtu.edu.cn/ubuntu/ precise-proposed main multiverse restricted universe
-deb http://mirror.bjtu.edu.cn/ubuntu/ precise-security main multiverse restricted universe
-deb http://mirror.bjtu.edu.cn/ubuntu/ precise-updates main multiverse restricted universe
-deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise main multiverse restricted universe
-deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-backports main multiverse restricted universe
-deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-proposed main multiverse restricted universe
-deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-security main multiverse restricted universe
-deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-updates main multiverse restricted universe
-EOF
-
-## enable apt-get --force-yes option
-cat <<EOF >> /home/vagrant/devstack/functions
-function apt_get() {
-    [[ "$OFFLINE" = "True" || -z "$@" ]] && return
-    local sudo="sudo"
-    [[ "$(id -u)" = "0" ]] && sudo="env"
-    $sudo DEBIAN_FRONTEND=noninteractive \
-    http_proxy=$http_proxy https_proxy=$https_proxy \
-    no_proxy=$no_proxy \
-    apt-get --force-yes --option "Dpkg::Options::=--force-confold" --assume-yes "$@"
-}
-EOF
+## The following will replace the repo source for apt-get
+# cp /etc/apt/sources.list{,.$(date +%F_%T)}
+# cat <<EOF > /etc/apt/sources.list
+# deb http://mirror.bjtu.edu.cn/ubuntu/ precise main multiverse restricted universe
+# deb http://mirror.bjtu.edu.cn/ubuntu/ precise-backports main multiverse restricted universe
+# deb http://mirror.bjtu.edu.cn/ubuntu/ precise-proposed main multiverse restricted universe
+# deb http://mirror.bjtu.edu.cn/ubuntu/ precise-security main multiverse restricted universe
+# deb http://mirror.bjtu.edu.cn/ubuntu/ precise-updates main multiverse restricted universe
+# deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise main multiverse restricted universe
+# deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-backports main multiverse restricted universe
+# deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-proposed main multiverse restricted universe
+# deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-security main multiverse restricted universe
+# deb-src http://mirror.bjtu.edu.cn/ubuntu/ precise-updates main multiverse restricted universe
+# EOF
